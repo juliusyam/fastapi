7deb2784ed5e0192bc5bf036976967407706e238
juliusyam <juliusyam@users.noreply.github.com>
2021-12-30 12:08:51 +0000

    use environmental variables

--- app/database.py
+++ app/database.py
@@ -5,8 +5,10 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost/fastapi'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@' \
+                          f'{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -23,15 +25,15 @@ def get_db():
         db.close()
 
 
-while True:
-
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
-                                password='password123', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection established")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
+# while True:
+#
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+#                                 password='password123', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection established")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
 from . import models
+from .config import settings
 from .database import engine
 from .routers import post, user, auth
+
 
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+
+print(settings.secret_key)
 
 
 app.include_router(auth.router)
