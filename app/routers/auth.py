abe853af8064c371c8dee1a11924de98abdf8591
juliusyam <juliusyam@users.noreply.github.com>
2022-01-08 13:38:04 +0000

    update api to user OAuthPasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,35 +1,18 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi import Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from .. import schemas, models, utils, oauth2
 from ..database import get_db
 
-router = APIRouter(
-    tags=["Authentication"]
+tags=["Authentication"]
 )
 
 
-@router.post("/login", response_model=schemas.Token)
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
-
-    if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
-
-    access_token = oauth2.create_access_token(data={"user_id": user.id})
-
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-# @router.post("/login")
-# def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+# @router.post("/login", response_model=schemas.Token)
+# def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
 #
-#     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+#     user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
 #
 #     if not user:
 #         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
@@ -40,3 +23,19 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
 #     access_token = oauth2.create_access_token(data={"user_id": user.id})
 #
 #     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/login", response_model=schemas.Token)
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+
+    if not utils.verify(user_credentials.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
